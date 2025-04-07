1ad0c1b2abaa7fa282223523e859c085bb2be1ec
codersjj <shanewestlife@outlook.com>
2025-04-07 09:09:10 +0800

    feat: Programatic Verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -18,4 +19,12 @@ module.exports = {
       chainId: 11155111,
     },
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  // sourcify: {
+  //   // Disabled by default
+  //   // Doesn't need an API key
+  //   enabled: true,
+  // },
 }
--- ignition/modules/SimpleStorage.js
+++ ignition/modules/SimpleStorage.js
@@ -11,9 +11,31 @@ async function main() {
   // what's the private key?
   // what's the rpc url?
 
-  console.log(
-    `SimpleStorage contract deployed to: ${await simpleStorageContract.getAddress()}`
-  )
+  const address = await simpleStorageContract.getAddress()
+  console.log(`SimpleStorage contract deployed to: ${address}`)
+
+  // what happens when we deploy to our hardhat network?
+  // console.log("hre.network:", hre.network)
+  if (
+    hre.network.config.chainId === 11155111 &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await simpleStorageContract.deploymentTransaction().wait(6)
+    await verify(address, [])
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...")
+  try {
+    // see: https://github.com/NomicFoundation/hardhat/blob/main/packages/hardhat-verify/src/internal/task-names.ts#L4
+    await hre.run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (error) {
+    console.log("Error verifying contract:", error)
+  }
 }
 
 main()
